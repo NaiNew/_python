0adc99250dd62e034d7252505ca72cf2a7ee5462
LE-S34 <r208790@yahoo.com.tw>
2020-05-13 20:41:55 +0800

    python

---
+++ lesson06/bar/readfile.py
@@ -0,0 +1,18 @@
+file = open('salary.txt.py', 'r', encoding='UTF-8')
+rows = file.readlines()
+print(rows)
+# 求薪資總和
+# 資料整理
+employees = []
+for row in rows:
+    data = row.split(", ")
+    dict = {'name':data[0], 'salary':int(data[1].strip('\n'))}
+    employees.append(dict)
+
+print(employees)
+
+sum = 0
+for emp in employees:
+    sum = sum + emp['salary']
+
+print(sum)
