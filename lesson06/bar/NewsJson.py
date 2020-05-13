b5064eb57ff79e5118b9046790b89d9931d3c096
LE-S34 <r208790@yahoo.com.tw>
2020-05-13 21:22:34 +0800

    python

--- lesson06/bar/NewsJson.py
+++ lesson06/bar/NewsJson.py
@@ -8,4 +8,12 @@ data = json.loads(requests.get(url).text)
 for d in data.get('data'):
     dict = {'title':d.get('title'), 'headlines':d.get('headlines')}
     news_list.append(dict)
-print(news_list)
+
+file = open('news3.txt', 'a', encoding = 'UTF-8')
+for news in news_list:
+    for head in news['headlines']:
+        if '貸款' in  head[1]:
+            print(head)
+            file.writelines(head)
+            file.write('\n')
+
